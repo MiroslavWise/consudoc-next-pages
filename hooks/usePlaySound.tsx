e4a9b7dea7dcd2126598e18bbe7c2b74148e2e77
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2023-10-04 13:28:01 +0300

    add fix sound hooks

--- hooks/usePlaySound.tsx
+++ hooks/usePlaySound.tsx
@@ -1,79 +1,79 @@
 import { useEffect, useState } from "react"
 
 const usePlaySound = () => {
-    const [audiosWeWantToUnlock, setAudiosWeWantToUnlock] = useState<HTMLAudioElement[]>([])
-    const audioCtx = new AudioContext()
+    // const [audiosWeWantToUnlock, setAudiosWeWantToUnlock] = useState<HTMLAudioElement[]>([])
+    // const audioCtx = new AudioContext()
 
-    useEffect(() => {
-        setAudiosWeWantToUnlock((prev) => [...prev, new Audio("../../sound/nothing.wav")])
+    // useEffect(() => {
+    //     setAudiosWeWantToUnlock((prev) => [...prev, new Audio("../../sound/nothing.wav")])
 
-        const isTouched = () => {
-            if (audiosWeWantToUnlock.length) {
-                for (const audio of audiosWeWantToUnlock) {
-                    const source = audioCtx.createMediaElementSource(audio)
-                    source.connect(audioCtx.destination)
-                    audio.play()
-                    audio.pause()
-                    audio.currentTime = 0
-                }
-                setAudiosWeWantToUnlock([])
-            }
-        }
+    //     const isTouched = () => {
+    //         if (audiosWeWantToUnlock.length) {
+    //             for (const audio of audiosWeWantToUnlock) {
+    //                 const source = audioCtx.createMediaElementSource(audio)
+    //                 source.connect(audioCtx.destination)
+    //                 audio.play()
+    //                 audio.pause()
+    //                 audio.currentTime = 0
+    //             }
+    //             setAudiosWeWantToUnlock([])
+    //         }
+    //     }
 
-        window?.addEventListener("touchstart", isTouched, false)
+    //     window?.addEventListener("touchstart", isTouched, false)
 
-        const nothing = new Audio("../../sound/nothing.wav")
+    //     const nothing = new Audio("../../sound/nothing.wav")
 
-        nothing
-            .play()
-            .then(() => {
-                console.log("Audio is playing")
-            })
-            .catch(() => {})
-        return () => {
-            window?.removeEventListener("touchstart", isTouched)
-        }
-    }, [])
+    //     nothing
+    //         .play()
+    //         .then(() => {
+    //             console.log("Audio is playing")
+    //         })
+    //         .catch(() => {})
+    //     return () => {
+    //         window?.removeEventListener("touchstart", isTouched)
+    //     }
+    // }, [])
 
     const playSoundSwitchStatus = () => {
-        const onlineStatusSound = "../../sound/new_message_tone.mp3"
-        setAudiosWeWantToUnlock((prev) => [...prev, new Audio(onlineStatusSound)])
-        if (audiosWeWantToUnlock.length) {
-            for (const audio of audiosWeWantToUnlock) {
-                const source = audioCtx.createMediaElementSource(audio)
-                source.connect(audioCtx.destination)
-                audio.play()
-                audio.pause()
-                audio.currentTime = 0
-            }
-            setAudiosWeWantToUnlock([])
-        }
-        console.log("Sound 111111111111")
+        // const onlineStatusSound = "../../sound/new_message_tone.mp3"
+        // setAudiosWeWantToUnlock((prev) => [...prev, new Audio(onlineStatusSound)])
+        // if (audiosWeWantToUnlock.length) {
+        //     for (const audio of audiosWeWantToUnlock) {
+        //         const source = audioCtx.createMediaElementSource(audio)
+        //         source.connect(audioCtx.destination)
+        //         audio.play()
+        //         audio.pause()
+        //         audio.currentTime = 0
+        //     }
+        //     setAudiosWeWantToUnlock([])
+        // }
+        // console.log("Sound 111111111111")
     }
 
     const incomingDoctorCall = () => {
-        const callSound = "../../sound/zvuk-skayp-skype-call-calling-23010.wav"
-        setAudiosWeWantToUnlock((prev) => [...prev, new Audio(callSound)])
-        if (audiosWeWantToUnlock.length) {
-            for (const audio of audiosWeWantToUnlock) {
-                const source = audioCtx.createMediaElementSource(audio)
-                source.connect(audioCtx.destination)
-                audio.play()
-                audio.pause()
-                audio.currentTime = 0
-            }
-            setAudiosWeWantToUnlock([])
-        }
+        // const callSound = "../../sound/zvuk-skayp-skype-call-calling-23010.wav"
+        // setAudiosWeWantToUnlock((prev) => [...prev, new Audio(callSound)])
+        // if (audiosWeWantToUnlock.length) {
+        //     for (const audio of audiosWeWantToUnlock) {
+        //         const source = audioCtx.createMediaElementSource(audio)
+        //         source.connect(audioCtx.destination)
+        //         audio.play()
+        //         audio.pause()
+        //         audio.currentTime = 0
+        //     }
+        //     setAudiosWeWantToUnlock([])
+        // }
     }
 
     function stop() {
-        if (audiosWeWantToUnlock.length) {
-            for (const audio of audiosWeWantToUnlock) {
-                audio.pause()
-                audio.currentTime = 0
-            }
-            setAudiosWeWantToUnlock([])
-        }
+        // if (audiosWeWantToUnlock.length) {
+        //     for (const audio of audiosWeWantToUnlock) {
+        //         audio.pause()
+        //         audio.currentTime = 0
+        //     }
+        //     setAudiosWeWantToUnlock([])
+        // }
     }
 
     return { playSoundSwitchStatus, incomingDoctorCall, stop }
